7f77a1ab7d18af0da8b5540e12b95a63ddfcc8af
Gustavo Nycholas <gustavonycholas@gmail.com>
2022-07-31 23:18:37 -0300

    Sistema de Login simples
    usando as funções
    - IF | AND | ELSE

---
+++ main.py
@@ -0,0 +1,10 @@
+login = input('digite seu login: ')
+Senha = input('digite sua senha: ')
+
+Nome = 'gustavo'
+Senha_id = '1234'
+
+if login == Nome and Senha == Senha_id:
+    print('Logado com sucesso!')
+else:
+    print('Login ou Senha Inválidos')
